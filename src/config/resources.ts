a91c1e03c163d2ad4a026a9ca3da6c3d87efc76a
Chinsanaa <61517941+Chinsanaa@users.noreply.github.com>
2026-06-30 17:34:15 +0800

    Update resources.ts

--- src/config/resources.ts
+++ src/config/resources.ts
@@ -29,9 +29,9 @@ export const URLS = {
     trading: "https://github.com/Chinsanaa/Trading",
   },
   certificates: {
-    claudeCodeInAction: "https://www.linkedin.com/safety/go/?url=https%3A%2F%2Fverify.skilljar.com%2Fc%2Fk6t2seqvaquz&urlhash=g6nM&mt=VpwLliAivS6SKSOzYV8HLLoJ-si_aN2SLjcujm1PgN-rXRKKsclV_KUAjZy50u-x9oTYiMHAD8ykxSfFBBbFcHeCtso6&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3B3W2RrbfKTj%2BrVZ1t8v2Z7Q%3D%3D",
-    bloombergFinanceFundamentals: "https://www.linkedin.com/safety/go/?url=https%3A%2F%2Fportal.bloombergforeducation.com%2Fcertificates%2FG5nAGjRned4SHRB5geGQURAR&urlhash=KhKY&mt=sj7QqWPDPNc7Kw6M_65mnccT5NYtbu9P3AxTQPMlZ3BqPojydLHbGtWMU59znqT7juWlPqtW93BBbLDgo7oCkj0IIAOX&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3B3W2RrbfKTj%2BrVZ1t8v2Z7Q%3D%3D",
-    googleAiEssentials: "hhttps://www.linkedin.com/safety/go/?url=https%3A%2F%2Fwww.coursera.org%2Faccount%2Faccomplishments%2Fspecialization%2FDJV0EELAKXYN&urlhash=w5_j&mt=8NsFe92bAdWVEQclFu27c4vHnS2BiII4U7-poEOpSKtEuEQgs6JX_YWK9A65m-lR81tRfdU1iRuSyQjn9X2W3XqE4UIr&isSdui=true&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3B3W2RrbfKTj%2BrVZ1t8v2Z7Q%3D%3D",
+    claudeCodeInAction: "https://verify.skilljar.com/c/k6t2seqvaquz",
+    bloombergFinanceFundamentals: "https://portal.bloombergforeducation.com/certificates/G5nAGjRned4SHRB5geGQURAR",
+    googleAiEssentials: "https://www.coursera.org/account/accomplishments/specialization/DJV0EELAKXYN",
   },
 } as const;
 
